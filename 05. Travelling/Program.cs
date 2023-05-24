15e7a6ad6e779daa4b536a7d009c62258575aa6f
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-24 04:21:03 +0300

    Add files via upload

---
+++ 05. Travelling/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+
+                if (destination == "End")
+                {
+                    break;
+                }
+
+                double requiredBudget = double.Parse(Console.ReadLine());
+                double savings = 0.0;
+
+                while (savings < requiredBudget)
+                {
+                    double sum = double.Parse(Console.ReadLine());
+                    savings += sum;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+            }
+        }
+    }
+}
